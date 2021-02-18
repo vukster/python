4db4fe718556599852bfd2a53a1f6f6e8fa0dd41
Daniel Vukicevic <dvukicev@cisco.com>
2021-02-18 16:39:17 +0100

    Update

---
+++ mood.py
@@ -0,0 +1,22 @@
+
+
+### Mood Checker ###
+mood = input ("Hello, in which mood are you today?")
+
+print (mood)
+
+if mood == "happy":
+    print ("It is great to see you happy!")
+elif mood == "nervous":
+    print ("Take a deep breath 3 times.")
+elif mood == "excited":
+    print ("You are excited uh?.")
+elif mood == "relaxed":
+    print("Nice you're relaxed.")
+else:
+    print("I don't recognize this mood")
+
+
+
+
+
